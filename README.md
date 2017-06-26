d90678fcbf00a5857b52087261b4f72bae12b143
suxiangxiao <13751882497@163.com>
2017-06-26 16:53:45 +0800

    no message

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # XSEncryptManager
-加密管理者
+加密管理者,目前只支持16位DES加密    
+
+1.cocoapod引入：    
+  pod 'XSEncryptManager', :git => 'https://github.com/xingshuaiEducation/XSEncryptManager.git'    
+
+2.引入头文件：    
+  import <XSEncryptManager.h>    
+
+3.使用代码：    
+  加密：    
+  NSString *ceshi = [DESUtil encryptUseDES:@"迪奥" key:@"1234567812345678"];    
+  解密：    
+  ceshi = [DESUtil decryptUseDES:ceshi key:@"1234567812345678"];
--- XSEncryptManager/XSEncryptManager.h
+++ XSEncryptManager/XSEncryptManager.h
@@ -17,9 +17,22 @@
 /********************* DES 加密解密 *************************/
 @interface DESUtil : NSObject
 
-//DES加密
+/**
+ DES加密
+
+ @param clearText 需要加密的内容
+ @param key 加密的密钥，为16
+ @return 加密后的内容
+ */
 +(NSString *) encryptUseDES:(NSString *)clearText key:(NSString *)key;
-//DES解密
+
+/**
+ DES解密
+
+ @param cipherText 需要解密的内容
+ @param key 解密的16位密钥
+ @return 解密后的内容
+ */
 +(NSString*) decryptUseDES:(NSString*)cipherText key:(NSString*)key;
 
 @end
